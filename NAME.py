a1489fe88f3714ff163529c2bb155418b0ce505d
Ryu <135315021+NotKaede-11@users.noreply.github.com>
2024-08-28 13:24:00 +0800

    NAME_UPDATE.py
    I did a little tweaking to make it look better.

--- NAME.py
+++ NAME.py
@@ -1,7 +1,12 @@
-print("""
+#ASCII ART FONT: Basic
+#LINK: https://patorjk.com/software/taag/#p=display&f=Basic&t=BEOMARC%20ANDREW%20D%20CARTONEROS
+
+i_am = """
 d8888b. d88888b  .d88b.  .88b  d88.  .d8b.  d8888b.  .o88b.       .d8b.  d8b   db d8888b. d8888b. d88888b db   d8b   db      d8888b.       .o88b.  .d8b.  d8888b. d888888b  .d88b.  d8b   db d88888b d8888b.  .d88b.  .d8888. 
 88  `8D 88'     .8P  Y8. 88'YbdP`88 d8' `8b 88  `8D d8P  Y8      d8' `8b 888o  88 88  `8D 88  `8D 88'     88   I8I   88      88  `8D      d8P  Y8 d8' `8b 88  `8D `~~88~~' .8P  Y8. 888o  88 88'     88  `8D .8P  Y8. 88'  YP 
 88oooY' 88ooooo 88    88 88  88  88 88ooo88 88oobY' 8P           88ooo88 88V8o 88 88   88 88oobY' 88ooooo 88   I8I   88      88   88      8P      88ooo88 88oobY'    88    88    88 88V8o 88 88ooooo 88oobY' 88    88 `8bo.   
 88~~~b. 88~~~~~ 88    88 88  88  88 88~~~88 88`8b   8b           88~~~88 88 V8o88 88   88 88`8b   88~~~~~ Y8   I8I   88      88   88      8b      88~~~88 88`8b      88    88    88 88 V8o88 88~~~~~ 88`8b   88    88   `Y8b. 
 88   8D 88.     `8b  d8' 88  88  88 88   88 88 `88. Y8b  d8      88   88 88  V888 88  .8D 88 `88. 88.     `8b d8'8b d8'      88  .8D      Y8b  d8 88   88 88 `88.    88    `8b  d8' 88  V888 88.     88 `88. `8b  d8' db   8D 
-Y8888P' Y88888P  `Y88P'  YP  YP  YP YP   YP 88   YD  `Y88P'      YP   YP VP   V8P Y8888D' 88   YD Y88888P  `8b8' `8d8'       Y8888D'       `Y88P' YP   YP 88   YD    YP     `Y88P'  VP   V8P Y88888P 88   YD  `Y88P'  `8888Y' """)
+Y8888P' Y88888P  `Y88P'  YP  YP  YP YP   YP 88   YD  `Y88P'      YP   YP VP   V8P Y8888D' 88   YD Y88888P  `8b8' `8d8'       Y8888D'       `Y88P' YP   YP 88   YD    YP     `Y88P'  VP   V8P Y88888P 88   YD  `Y88P'  `8888Y'                                                                                                                                                                                                                    """
+
+print(i_am)
